99d349bac6ff5ed34feb016ec88736e05e9b3c99
Radek2894 <85341631+Radek2894@users.noreply.github.com>
2022-04-03 22:18:54 +0200

    Dodaj pliki projektów.

---
+++ EFDemo/Models/Data/PeopleContext.cs
@@ -0,0 +1,12 @@
+﻿using EFDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+    public class PeopleContext : DbContext
+    {
+    public PeopleContext(DbContextOptions options) : base(options) { }
+    public DbSet<Person> Person { get; set; }
+    
+    }
+
+
